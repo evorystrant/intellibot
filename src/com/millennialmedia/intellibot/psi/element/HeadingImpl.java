307f09a787cc2204fc5fb5675242639e8c3d54a1
yu@sec.com <yu@sec.com>
2018-05-04 11:45:55 +0800

    fix error when have "keywords.robot"

--- src/com/millennialmedia/intellibot/psi/element/HeadingImpl.java
+++ src/com/millennialmedia/intellibot/psi/element/HeadingImpl.java
@@ -8,6 +8,8 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.containers.MultiMap;
 import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyFile;
+import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.psi.stubs.PyFunctionNameIndex;
 import com.jetbrains.python.psi.stubs.PyModuleNameIndex;
 import com.millennialmedia.intellibot.ide.icons.RobotIcons;
 import com.millennialmedia.intellibot.psi.dto.ImportType;
@@ -384,6 +386,7 @@ public class HeadingImpl extends RobotPsiElementBase implements Heading {
             }
         }
         findChildrenClass(files, "keywords");
+        // forcePatch(files);
         return files;
     }
 
@@ -392,53 +395,56 @@ public class HeadingImpl extends RobotPsiElementBase implements Heading {
      * search sibling library by keywords
      * patch for SeleniumLibrary dynamic keywords
      * support all libraries that contain "keywords" package.
-     * @param files  all pyClass
-     * @param libraryName  default search is keywords.
+     *
+     * @param files       all pyClass
+     * @param libraryName default search is keywords.
      */
-    void findChildrenClass(Collection files,String libraryName){
-//        // this is temporary patch,each patch return All keywords from file
-        // Force Patch by Selenium
-//        String[] sourcelist = {
-//                "open_browser",
-//                "get_cookies",
-//                "input_text_into_prompt",
-//                "get_webelement",
-//                "submit_form",
-//                "select_frame",
-//                "execute_javascript",
-//                "register_keyword_to_run_on_failure",
-//                "set_screenshot_directory",
-//                "get_list_items",
-//                "get_table_cell",
-//                "wait_for_condition",
-//                "active_drivers",
-//                "create_driver",
-//                "select_window"
-//        };
-//        for (String str : sourcelist) {
-//            Collection<PyFunction> funcs = PyFunctionNameIndex.find(str, getProject());
-//            for (PyFunction pyfunc : funcs) {
-//                PyClass cs = pyfunc.getContainingClass();
-//                files.add(new RobotPythonClass(cs.getName(), cs, ImportType.LIBRARY));
-//            }
-//        }
+    void findChildrenClass(Collection files, String libraryName) {
         Collection<PyFile> fileList = PyModuleNameIndex.find(libraryName, getProject(), true);
         List<PsiFile> allFiles = new ArrayList<PsiFile>();
         for (PyFile pyFile : fileList) {
-            if (pyFile.getParent() != null){
+            if (pyFile.getParent() != null) {
                 allFiles.addAll(Arrays.asList(pyFile.getParent().getFiles()));
             }
         }
-        for (PsiFile pyFile : allFiles) {
-            PsiFile nextFile = pyFile;
-            while (nextFile != null) {
-                PsiElement[] all = nextFile.getChildren();
-                for (PsiElement psiElement : all) {
-                    if (psiElement instanceof PyClass) {
-                        files.add(new RobotPythonClass(((PyClass) psiElement).getName(), (PyClass) psiElement, ImportType.LIBRARY));
-                    }
+        for (PsiFile psiFile : allFiles) {
+            // this is static library ,do not need to gen
+            if (psiFile.getNextSibling() instanceof RobotFileImpl) {
+                continue;
+            }
+            PsiElement[] all = psiFile.getChildren();
+            for (PsiElement psiElement : all) {
+                if (psiElement instanceof PyClass) {
+                    files.add(new RobotPythonClass(((PyClass) psiElement).getName(), (PyClass) psiElement, ImportType.LIBRARY));
                 }
-                nextFile = (PyFile) nextFile.getNextSibling();
+            }
+        }
+    }
+
+    public void forcePatch(Collection files) {
+        //Force Patch by Selenium
+        String[] sourcelist = {
+                "open_browser",
+                "get_cookies",
+                "input_text_into_prompt",
+                "get_webelement",
+                "submit_form",
+                "select_frame",
+                "execute_javascript",
+                "register_keyword_to_run_on_failure",
+                "set_screenshot_directory",
+                "get_list_items",
+                "get_table_cell",
+                "wait_for_condition",
+                "active_drivers",
+                "create_driver",
+                "select_window"
+        };
+        for (String str : sourcelist) {
+            Collection<PyFunction> funcs = PyFunctionNameIndex.find(str, getProject());
+            for (PyFunction pyfunc : funcs) {
+                PyClass cs = pyfunc.getContainingClass();
+                files.add(new RobotPythonClass(cs.getName(), cs, ImportType.LIBRARY));
             }
         }
     }
